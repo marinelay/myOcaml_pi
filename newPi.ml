441e9bb5a35660f091ef73eb2b2ca64cd61337f8
Oh Wonseok <marinelay7@gmail.com>
2019-07-27 08:06:31 +0900

    Maybe some add...

--- newPi.ml
+++ newPi.ml
@@ -1,4 +1,5 @@
 type exp =
+  | UNIT
   | INT of int
   | TRUE | FALSE
   | VAR of var
@@ -8,11 +9,13 @@ type exp =
   | LE of exp * exp
   | GT of exp * exp
   | GE of exp * exp
+  | ASSIGN of var * exp
   | SEQ of exp * exp
 and var = string
 ;;
 
 type value =
+  | Unit
   | Int of int
   | Bool of bool
   (* symbol *)
@@ -32,7 +35,7 @@ type sym_exp =
   | EQ of value * value
   | NOTEQ of value * value
   | LESSTHAN of value * value
-  | LESSEQAUL of value * value
+  | LESSEQUAL of value * value
   | GREATTHAN of value * value
   | GREATEQUAL of value * value
 and path_cond = sym_exp
@@ -49,26 +52,105 @@ let rec apply_env env x =
   | [] -> raise(Failure "None in env")
   | (y,v)::tl -> if y=x then v else apply_env tl x
 
+  (* 타입 맞춰주기 *)
+  (*
+  * if문을 예로 들면, condition을 받았다고 해보자
+  * condition을 eval_exp하면 (value * path_cond) list가 나올테고
+  * list 원소들의 value를 뽑아내야 하므로 eval_exp_aux를 통해
+  * value와 path_cond을 뽑아내는 것이다
+  *)
+let rec eval_exp_aux : (value * path_cond) list -> (value -> path_cond -> (value * path_cond) list) -> (value * path_cond) list
+= fun l f ->
+  match l with
+  | [] -> []
+  | (y, pi)::tl -> (f y pi)@(eval_exp_aux tl f)
+
 let rec eval_exp : exp -> env -> path_cond -> (value * path_cond) list
 = fun exp env pi ->
   match exp with
+  | UNIT -> [(Unit, pi)]
   | INT n -> [(Int n, pi)]
+  | TRUE -> [(Bool true, pi)]
+  | FALSE -> [(Bool false, pi)]
   | VAR v -> [(apply_env env v, pi)]
-  | IF (cond, body1, body2) -> (* match 수정필요 *)
-    let c = eval_exp cond env pi in
-    (match c with
-    | Bool b -> eval_exp (if b then body1 else body2) env pi
-    | SBool _ -> (eval_exp body1 env (AND(pi, EQ(c, Bool true))))@
-    (eval_exp body2 env (AND(pi, EQ(c, Bool false))))
-    )
+  | IF (cond, body1, body2) ->
+    let l = eval_exp cond env pi in
+      eval_exp_aux l (fun v pi ->
+        (match v with
+        | Bool b -> eval_exp (if b then body1 else body2) env pi
+        | SBool _ -> (eval_exp body1 env (AND(pi, EQ(v, Bool true))))@
+        (eval_exp body2 env (AND(pi, EQ(v, Bool false))))
+        | _ -> raise(Failure "Type Error : IF")
+        )
+      )
   | ADD (e1, e2) ->
-    let v1 = eval_exp e1 env pi in
-    let v2 = eval_exp e2 env pi in
-    (match (v1,v2) with
-    | Bool _, _ | SBool _, _
-    | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : ADD")
-    | Int n1, Int n2 -> [(Int (n1+n2), pi)]
-    | _ -> [(SArith(SADD, n1, n2), pi)]
-    )
+    let l1 = eval_exp e1 env pi in
+      eval_exp_aux l1 (fun v1 pi ->
+        let l2 = eval_exp e2 env pi in
+          eval_exp_aux l2 (fun v2 pi ->
+            (match (v1,v2) with
+            | Bool _, _ | SBool _, _
+            | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : ADD")
+            | Int n1, Int n2 -> [(Int (n1+n2), pi)]
+            | _ -> [(SArith(SADD, v1, v2), pi)]
+            )
+          )
+      )
+
+  | LT (e1, e2) ->
+    let l1 = eval_exp e1 env pi in
+      eval_exp_aux l1 (fun v1 pi ->
+        let l2 = eval_exp e2 env pi in
+          eval_exp_aux l2 (fun v2 pi ->
+            (match (v1, v2) with
+            | Bool _, _ | SBool _, _
+            | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : LT")
+            | Int n1, Int n2 -> [(Bool (n1 < n2), pi)]
+            | _ -> [(Bool true, AND(pi, LESSTHAN(v1, v2))); (Bool false, AND(pi, GREATEQUAL(v1, v2)))]
+            )
+          )
+      )
+  | LE (e1, e2) ->
+    let l1 = eval_exp e1 env pi in
+      eval_exp_aux l1 (fun v1 pi ->
+        let l2 = eval_exp e2 env pi in
+          eval_exp_aux l2 (fun v2 pi ->
+            (match (v1, v2) with
+            | Bool _, _ | SBool _, _
+            | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : LT")
+            | Int n1, Int n2 -> [(Bool (n1 <= n2), pi)]
+            | _ -> [(Bool true, AND(pi, LESSEQUAL(v1, v2))); (Bool false, AND(pi, GREATTHAN(v1, v2)))]
+            )
+          )
+      )
+  | GT (e1, e2) ->
+    let l1 = eval_exp e1 env pi in
+      eval_exp_aux l1 (fun v1 pi ->
+        let l2 = eval_exp e2 env pi in
+          eval_exp_aux l2 (fun v2 pi ->
+            (match (v1, v2) with
+              | Bool _, _ | SBool _, _
+              | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : LT")
+              | Int n1, Int n2 -> [(Bool (n1 > n2), pi)]
+              | _ -> [(Bool true, AND(pi, GREATTHAN(v1, v2))); (Bool false, AND(pi, LESSEQUAL(v1, v2)))]
+            )
+          )
+      )
+  | GE (e1, e2) ->
+    let l1 = eval_exp e1 env pi in
+      eval_exp_aux l1 (fun v1 pi ->
+        let l2 = eval_exp e2 env pi in
+          eval_exp_aux l2 (fun v2 pi ->
+            (match (v1, v2) with
+            | Bool _, _ | SBool _, _
+            | _, Bool _ | _, SBool _ -> raise(Failure "Type Error : LT")
+            | Int n1, Int n2 -> [(Bool (n1 >= n2), pi)]
+            | _ -> [(Bool true, AND(pi, GREATEQUAL(v1, v2))); (Bool false, AND(pi, LESSTHAN(v1, v2)))]
+            )
+          )
+      )
+  | ASSIGN (x, e) -> 
+    let l = eval_exp e env pi in
+      eval_exp_aux l (fun v pi -> eval_exp x (append_env env (x, v) pi))
   | SEQ (e1, e2) ->
-    [(let _ = eval_env e1 env pi in eval_env e2 env pi, pi)]  
+    let _ = eval_exp e1 env pi in eval_exp e2 env pi
